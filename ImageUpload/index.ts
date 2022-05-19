ba048e5e4bd48db5c9cfac4e7a2f498a2c3928a2
Paweł Figiel <pfigiel@future-processing.com>
2022-05-19 15:27:52 +0200

    Added prettier formatting.

--- ImageUpload/index.ts
+++ ImageUpload/index.ts
@@ -5,51 +5,34 @@ import * as sharp from "sharp";
 import imageType from "image-type";
 import { v4 as uuidv4 } from "uuid";
 import * as exif from "exif-js";
-import { Blob } from "node:buffer"
+import { Blob } from "node:buffer";
 
 global.Blob = Blob as any;
 
-const httpTrigger: AzureFunction = async (
-  context: Context,
-  req: HttpRequest
-): Promise<void> => {
+const httpTrigger: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
   try {
     if (!req.body) {
       return endWithBadResponse(context);
     }
 
-    const parts = parse(
-      Buffer.from(req.body),
-      getBoundary(req.headers["content-type"])
-    );
+    const parts = parse(Buffer.from(req.body), getBoundary(req.headers["content-type"]));
 
-    if (
-      parts.length !== 2 ||
-      !parts[0].filename ||
-      parts[1].name !== "userId"
-    ) {
+    if (parts.length !== 2 || !parts[0].filename || parts[1].name !== "userId") {
       return endWithBadResponse(context);
     }
 
     const imageInput = parts[0];
     const imageTypeInfo = imageType(imageInput.data);
-    const imageBlobStorageName = `${uuidv4()}.${imageTypeInfo.ext}`
+    const imageBlobStorageName = `${uuidv4()}.${imageTypeInfo.ext}`;
 
-    const thumbnail = await sharp(imageInput.data)
-      .resize(200, 200)
-      .withMetadata()
-      .toBuffer();
+    const thumbnail = await sharp(imageInput.data).resize(200, 200).withMetadata().toBuffer();
 
-    const blobServiceClient = BlobServiceClient.fromConnectionString(
-      process.env.BlobStorageConnectionString
-    );
+    const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.BlobStorageConnectionString);
 
     const containerClient = blobServiceClient.getContainerClient("images");
     await containerClient.createIfNotExists();
 
-    const imageBlobClient = containerClient.getBlockBlobClient(
-      imageBlobStorageName
-    );
+    const imageBlobClient = containerClient.getBlockBlobClient(imageBlobStorageName);
     await imageBlobClient.uploadData(parts[0].data, {
       blobHTTPHeaders: { blobContentType: imageTypeInfo.mime },
     });
@@ -60,27 +43,29 @@ const httpTrigger: AzureFunction = async (
     });
 
     const imageMetadata = exif.readFromBinaryFile(imageInput.data.buffer);
-    
+
     const latitudeDMS = imageMetadata.GPSLatitude;
     const latitudeDirection = imageMetadata.GPSLatitudeRef;
     const longitudeDMS = imageMetadata.GPSLongitude;
     const longitudeDirection = imageMetadata.GPSLongitudeRef;
 
-    const latitude = latitudeDMS?.length === 3 && ["N", "S"].includes(latitudeDirection)
-      ? dmsToDD(latitudeDMS[0], latitudeDMS[1], latitudeDMS[2], latitudeDirection)
-      : undefined;
-    const longitude = longitudeDMS?.length === 3 && ["E", "W"].includes(longitudeDirection)
-      ? dmsToDD(longitudeDMS[0], longitudeDMS[1], longitudeDMS[2], longitudeDirection)
-      : undefined;
-    
+    const latitude =
+      latitudeDMS?.length === 3 && ["N", "S"].includes(latitudeDirection)
+        ? dmsToDD(latitudeDMS[0], latitudeDMS[1], latitudeDMS[2], latitudeDirection)
+        : undefined;
+    const longitude =
+      longitudeDMS?.length === 3 && ["E", "W"].includes(longitudeDirection)
+        ? dmsToDD(longitudeDMS[0], longitudeDMS[1], longitudeDMS[2], longitudeDirection)
+        : undefined;
+
     context.bindings.cosmosDbRes = JSON.stringify({
       imageUrl: imageBlobClient.url,
       thumbnailUrl: thumbnailBlobClient.url,
       userId: Buffer.from(parts[1].data).toString(),
       gpsCoordinates: {
         latitude,
-        longitude
-      }
+        longitude,
+      },
     });
 
     context.log("HTTP trigger function processed a request.");
@@ -100,14 +85,14 @@ const endWithBadResponse = (context, message = "Bad Request") => {
     body: message,
   };
   context.done();
-}
+};
 
 const dmsToDD = (degrees: number, minutes: number, seconds: number, direction: string) => {
   var dd = degrees + minutes / 60 + seconds / (60 * 60);
 
   if (direction === "S" || direction === "W") {
-      dd = -dd;
+    dd = -dd;
   }
-  
+
   return dd;
-}
+};
