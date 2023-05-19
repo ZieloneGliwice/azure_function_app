148a65af8188a8668ab5adffc2a47381358ae39f
mcalik <mcalik@future-processing.com>
2023-05-19 14:24:03 +0200

    extracted helper to get blob container name per env

--- common/connections.ts
+++ common/connections.ts
@@ -1,5 +1,6 @@
 import { CosmosClient } from "@azure/cosmos";
 import { ContainerClient, StorageSharedKeyCredential } from "@azure/storage-blob";
+import { getBlobContainerName } from ".";
 
 export const cosmosDbClient = new CosmosClient(process.env.CosmosDbConnectionString).database(process.env.CosmosDbName);
 
@@ -8,6 +9,6 @@ export const dictsCollection = cosmosDbClient.container("Dicts");
 export const treesCollection = cosmosDbClient.container("Trees");
 
 export const blobContainerClient = new ContainerClient(
-  `${process.env.BlobUrl}/images${process.env.Environment === "dev" ? "-dev" : ""}`,
+  `${process.env.BlobUrl}/${getBlobContainerName()}`,
   new StorageSharedKeyCredential(process.env.BlobAccountName, process.env.BlobAccountKey),
 );
