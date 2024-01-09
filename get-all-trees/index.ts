bb35f2813d6fd6648f1f8fd105eced54525dd7d7
filip <filipgawlas@gmail.com>
2024-01-09 11:35:37 +0100

    Updated endpoint to get all trees

--- get-all-trees/index.ts
+++ get-all-trees/index.ts
@@ -1,7 +1,21 @@
 import { AzureFunction, Context } from "@azure/functions";
-import { authorizeAdminAsync } from "../common";
+import { getContainerSasUri, getUserId } from "../common";
+import { treesCollection } from "../common/connections";
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
-  await authorizeAdminAsync(context);
+  const querySpec = {
+    query: `SELECT t.id
+                ,t.treeThumbnailUrl
+                ,t._ts
+                ,t.latLong
+                ,t.species
+            FROM Trees t`,
+  };
+
+  const { resources } = await treesCollection.items.query(querySpec).fetchAll();
+
+  context.res = {
+    body: { trees: resources, sasToken: resources.length > 0 ? getContainerSasUri() : undefined },
+  };
 };
 export default httpTrigger;
